b6efac7d555809749fcc0c3fd6df01ce923267cc
Ekaterina K <katevostruhina@mail.ru>
2024-02-28 22:07:11 +0300

    first commit

--- controller.py
+++ controller.py
@@ -1,42 +1,54 @@
 import view
-import model
+from model import PhoneBook
 import text
 
 
-def find_contacts(message):
-    search_word = view.input_data(text.message)
-    result = model.find_contact(search_word)
-    view.show_contacts(result, text.find_contact_no_result(search_word))
-    return True if result else False
+# def find_contacts(phone_book, message):
+#     search_word = view.input_data(text.message)
+#     result = phone_book.find_contact(search_word)
+#     view.show_contacts(result, text.find_contact_no_result(search_word))
+#     return True if result else False
 
 def srtart_app():
+    pb = PhoneBook()
     while True:
         user_choice = view.show_main_menu()
         match user_choice:
             case 1:
-                model.open_phone_book()
+                pb.open_phone_book()
                 view.show_message(text.phone_book_opened_successful)
             case 2:
-                model.save_phone_book()
+                pb.save_phone_book()
                 view.show_message(text.phone_book_saved_successful)
             case 3:
-                view.show_contacts(model.phone_book, text.empty_phone_book_error)
+                view.show_contacts(pb.phone_book, text.empty_phone_book_error)
             case 4:
                 new_contact = view.input_data(text.input_new_contact)
-                model.add_new_contact(new_contact)
+                pb.add_new_contact(new_contact)
                 view.show_message(text.new_contact_added_successful)
             case 5:
-                find_contacts(text.input_search_word)
+                pb.find_contacts(view.input_data, view.show_contacts, text.input_search_word, text.find_contscts_no_results)
             case 6:
-                if find_contacts(text.input_search_word_for_edit):
+                if pb.find_contacts(view.input_data, view.show_contacts, text.input_search_word_for_edit, text.find_contscts_no_results):
                     u_id = int(view.input_data(text.input_id_for_edit))
                     edited_contact = view.input_data(text.edit_contact)
-                    name = model.edit_contact(u_id, edited_contact)
+                    name = pb.edit_contact(u_id, edited_contact)
                     view.show_message(text.edit_contact_successful(name))
             case 7:
-                if find_contacts(text.input_search_word_for_delete):
+                if pb.find_contacts(view.input_data, view.show_contacts, text.input_search_word_for_delete, text.find_contscts_no_results):
                     u_id = int(view.input_data(text.input_id_for_delete))
-                    name = model.delete_contact(u_id)
+                    name = pb.delete_contact(u_id)
                     view.show_message(text.edit_contact_successful(name))
             case 8:
-                pass
+                if pb.on_exit():
+                    view.show_message(text.exit_changes)
+                    if view.input_data(text.exit_confirm).lower() == 'y':
+                        pb.save_phone_book()
+                        view.show_message(text.exit_no_changes_confirm)
+                    else:
+                        view.show_message(text.exit_no_changes_no_confirm)
+
+                
+                else:
+                    view.show_message(text.exit_no_changes)
+                break
